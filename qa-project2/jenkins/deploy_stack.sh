3d28875abd09cb89ddba7445ee7485c1cdd7e2dc
gergelykom <gerge@jenkins-vm.europe-west2-c.c.cool-component-296511.internal>
2021-01-22 17:09:32 +0000

    new

--- qa-project2/jenkins/deploy_stack.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-scp -i ~/.ssh/id_rsa docker-compose.yaml jenkins@swarm-manager:/home/gerge/qa-project2/qa-project2/docker-compose.yaml
-ssh -i ~/.ssh/id_rsa jenkins@swarm-manager << EOF
-    export DATABASE_URI=${DATABASE_URI}
-    
-    docker stack deploy --compose-file /home/gerge/qa-project2/qa-project2/docker-compose.yaml qa-project2
-EOF
