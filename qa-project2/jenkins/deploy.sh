7126bf5b83e3a1186228892723cd06adac4c7f66
gergelykom <gerge@jenkins-vm.europe-west2-c.c.cool-component-296511.internal>
2021-01-22 14:01:43 +0000

    more fixing

--- qa-project2/jenkins/deploy.sh
+++ qa-project2/jenkins/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-scp -i ~/.ssh/id_rsa docker-compose.yaml jenkins@swarm-master:/home/jenkins/docker-compose.yaml
-ssh -i ~/.ssh/id_rsa jenkins@swarm-master << EOF
+scp -i ~/.ssh/id_rsa docker-compose.yaml jenkins@swarm-manager:/home/jenkins/docker-compose.yaml
+ssh -i ~/.ssh/id_rsa jenkins@swarm-manager << EOF
     export DATABASE_URI=${DATABASE_URI} app_version=${app_version} 
     docker stack deploy --compose-file /home/jenkins/docker-compose.yaml qa-project2
 EOF
--- qa-project2/jenkins/deploy_stack.sh
+++ qa-project2/jenkins/deploy_stack.sh
@@ -2,6 +2,6 @@
 scp -i ~/.ssh/id_rsa docker-compose.yaml jenkins@swarm-manager:/home/jenkins/docker-compose.yaml
 ssh -i ~/.ssh/id_rsa jenkins@swarm-manager << EOF
     export DATABASE_URI=${DATABASE_URI}
-    export SECRET_KEY=${SECRET_KEY}
+    
     docker stack deploy --compose-file /home/jenkins/docker-compose.yaml qa-project2
 EOF
