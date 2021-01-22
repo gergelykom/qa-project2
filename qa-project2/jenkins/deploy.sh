82271fa3b25e50f26aa826b86529940fea100223
gergelykom <gerge@jenkins-vm.europe-west2-c.c.cool-component-296511.internal>
2021-01-22 14:52:22 +0000

    new

--- qa-project2/jenkins/deploy.sh
+++ qa-project2/jenkins/deploy.sh
@@ -2,5 +2,5 @@
 scp -i ~/.ssh/id_rsa docker-compose.yaml jenkins@swarm-manager:/home/jenkins/docker-compose.yaml
 ssh -i ~/.ssh/id_rsa jenkins@swarm-manager << EOF
     export DATABASE_URI=${DATABASE_URI} app_version=${app_version} 
-    docker stack deploy --compose-file /home/jenkins/docker-compose.yaml qa-project2
+    docker stack deploy --compose-file /home/gerge/qa-project2/qa-project2/docker-compose.yaml qa-project2
 EOF
--- qa-project2/jenkins/deploy_stack.sh
+++ qa-project2/jenkins/deploy_stack.sh
@@ -3,5 +3,5 @@ scp -i ~/.ssh/id_rsa docker-compose.yaml jenkins@swarm-manager:/home/jenkins/doc
 ssh -i ~/.ssh/id_rsa jenkins@swarm-manager << EOF
     export DATABASE_URI=${DATABASE_URI}
     
-    docker stack deploy --compose-file /home/jenkins/docker-compose.yaml qa-project2
+    docker stack deploy --compose-file /home/gerge/qa-project2/qa-project2/docker-compose.yaml qa-project2
 EOF
