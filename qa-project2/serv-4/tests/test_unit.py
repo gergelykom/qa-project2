b37a081603547728959699472491375f4c0d017d
gergelykom <gerge@jenkins-vm.europe-west2-c.c.cool-component-296511.internal>
2021-01-23 11:22:21 +0000

    more tests

--- qa-project2/serv-4/tests/test_unit.py
+++ qa-project2/serv-4/tests/test_unit.py
@@ -20,13 +20,14 @@ class TestResponse(TestBase):
 
     def test_whatudone(self):
         response = self.client.post(url_for('whatudone'), json={ 'meal' : 'pasta' , 'qran' : '2400'})
-        response = self.client.post(url_for('whatudone'), json={ 'meal' : 'pasta' , 'qran' : '1800'})
-        response = self.client.post(url_for('whatudone'), json={ 'meal' : 'burger' , 'qran' : '2400'})
-        response = self.client.post(url_for('whatudone'), json={ 'meal' : 'pasta' , 'qran' : '1800'})
+       
         
         self.assertEqual(b"You will roll 6 nat20's in a row!", response.data)
+        
+    def test_whatudone2(self):
+        response = self.client.post(url_fot('whatudone'), json={ 'meal' : 'pasta' , 'qran' : '1800'})
+        
         self.assertEqual(b"Congratulations, you just destroyed the Universe!", response.data)
-        self.assertEqual(b"You just helped humanity to achieve techonological singularity!", response.data)
-        self.assertEqual(b"You just aided the emergence of intelligent sushi!", response.data)
+        
         
 
