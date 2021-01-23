36299b2ca449ed35a4bb73523561e5bd875365a9
gergelykom <gerge@jenkins-vm.europe-west2-c.c.cool-component-296511.internal>
2021-01-23 18:03:09 +0000

    testing version 2, fixed issues

--- qa-project2/serv-2/tests/test_unit.py
+++ qa-project2/serv-2/tests/test_unit.py
@@ -19,10 +19,10 @@ class TestResponse(TestBase):
 
     def test_qran(self):
         with patch("random.choice") as random:
-            random.return_value = "4200"           
+            random.return_value = "4000"           
             response = self.client.get(url_for('get_qran'))
-            self.assertEqual(b'4200', response.data)
+            self.assertEqual(b'4000', response.data)
 
         for _ in range(10):
             response = self.client.get(url_for('get_qran'))
-            self.assertIn(response.data, [b"60", b"600", b"1800", b"2400", b"3600", b"4200"])
+            self.assertIn(response.data, [b"80", b"800", b"1600", b"2400", b"3200", b"4000"])
